ad14ac4aa76057e04bcfb48bea8457da890ec034
Andy-M7 <allataspariguana@gmail.com>
2025-11-02 15:24:29 -0500

    Creacion de carpetas

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,25 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
-      'react-native-dotenv',
+      'module:react-native-dotenv',
       {
         moduleName: '@env',
         path: '.env',
       },
     ],
+    [
+      'babel-plugin-module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.json'],
+        alias: {
+          '@domain': './src/domain',
+          '@data': './src/data',
+          '@presentation': './src/presentation',
+          '@components': './src/presentation/components',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
   ],
 };
