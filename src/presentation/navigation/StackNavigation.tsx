f75fac6d6648a091f935aea4bed1838f6bd4084a
Harold242001 <harold.berru24@gmail.com>
2025-11-02 22:03:40 -0500

    icono + login

--- src/presentation/navigation/StackNavigation.tsx
+++ src/presentation/navigation/StackNavigation.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Screens
+import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 import UsuariosScreen from '../screens/UsuariosScreen';
 import EmpleadosScreen from '../screens/EmpleadosScreen';
@@ -14,6 +15,7 @@ import RolesScreen from '../screens/RolesScreen';
 
 // ===== Tipado de rutas =====
 export type RootStackParamList = {
+  Login: undefined;
   Home: undefined;
   Usuarios: undefined;
   Empleados: undefined;
@@ -21,7 +23,7 @@ export type RootStackParamList = {
   Productos: undefined;
   Asistencia: undefined;
   Roles: undefined;
-  VisualizarAsistencia: { month?: string } | undefined; 
+  //VisualizarAsistencia: { month?: string } | undefined; // ej. '2025-11'
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -29,19 +31,26 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function StackNavigation() {
   return (
     <Stack.Navigator
-      initialRouteName="Home"
+      initialRouteName="Login"
       screenOptions={{
         headerTitleAlign: 'left',
         headerShadowVisible: false,
       }}
     >
+      {/* Auth */}
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+
+      {/* App */}
       <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Inicio' }} />
       <Stack.Screen name="Usuarios" component={UsuariosScreen} options={{ title: 'Usuarios' }} />
       <Stack.Screen name="Empleados" component={EmpleadosScreen} options={{ title: 'Empleados' }} />
       <Stack.Screen name="PackingLists" component={PackingListsScreen} options={{ title: 'Packing Lists' }} />
       <Stack.Screen name="Productos" component={ProductosScreen} options={{ title: 'Productos' }} />
       <Stack.Screen name="Asistencia" component={AsistenciaScreen} options={{ title: 'Asistencia' }} />
-      <Stack.Screen name="Roles" component={RolesScreen} options={{ title: 'Roles' }} />
     </Stack.Navigator>
   );
 }
